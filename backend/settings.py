250858b3d80d366b5ba5c7cb417568156ecbd3c2
Arezoo <arezoo.darvishi69@yahoo.com>
2024-04-14 23:32:03 +0330

    add apps into instaled apps in setting

--- backend/settings.py
+++ backend/settings.py
@@ -37,6 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # Custom Apps
+    'store',
+    'userauths',
+    'vendor',
+    'customer',
+    'api'
+    
 ]
 
 MIDDLEWARE = [
