541e32ad89e65d60c86ae565d664bd008c7bcb7b
Arezoo <arezoo.darvishi69@yahoo.com>
2024-04-16 10:03:01 +0330

    customize userauths admin panel

--- userauths/admin.py
+++ userauths/admin.py
@@ -2,5 +2,16 @@ from django.contrib import admin
 from userauths.models import User, Profile
 
 
-admin.site.register(User)
-admin.site.register(Profile)
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['full_name', 'email', 'phone']
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['get_full_name', 'gender']
+    list_editable = ['gender']
+    # search_fields = ['gender']
+
+    def get_full_name(self, obj):
+        return obj.user.full_name
