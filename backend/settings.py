42519c9908d88829af01421e6373a330486726fd
Arezoo <arezoo.darvishi69@yahoo.com>
2024-04-16 09:49:49 +0330

    define auth user model

--- backend/settings.py
+++ backend/settings.py
@@ -136,6 +136,8 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
+AUTH_USER_MODEL='userauths.User'
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 JAZZMIN_SETTINGS = {
